d6dd09a30121a7f486f8c78a3ff90f506c25cd23
FranMaric <fran.maric.vk@gmail.com>
2022-10-14 15:35:08 +0200

    Initial architecture

---
+++ SOFTWARE/flutter_client/lib/ui/common/bits/request_notifier/request_notifier.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:loggy/loggy.dart';
+
+import 'request_state.dart';
+
+abstract class RequestNotifier<Value> extends StateNotifier<RequestState<Value>> with NetworkLoggy {
+  RequestNotifier({RequestState<Value> initial = const RequestState.initial()}) : super(initial);
+
+  Future<void> executeRequest({
+    required ValueGetter<Future<Value>> requestBuilder,
+    Exception? Function(Exception)? errorHandler,
+  }) async {
+    try {
+      state = state.maybeMap(
+        success: (result) => RequestState.loading(resultMaybe: result.value),
+        orElse: () => RequestState<Value>.loading(),
+      );
+
+      final value = await requestBuilder();
+      state = RequestState.success(value);
+    } catch (error, st) {
+      loggy.error('Request Error', error, st);
+      final exception = (error is Exception) ? error : Exception();
+      final stateException = errorHandler != null ? errorHandler(exception) : exception;
+      if (stateException != null) {
+        state = RequestState.failure(stateException);
+      } else {
+        state = const RequestState.initial();
+      }
+    }
+  }
+
+  void reset() => state = const RequestState.initial();
+}
