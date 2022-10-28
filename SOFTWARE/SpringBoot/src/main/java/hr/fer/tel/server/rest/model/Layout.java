adabaddafc761870bddb62b94722811553d11d6a
lmakovec <lovro.makovec@fer.hr>
2022-10-28 12:49:34 +0200

    dodan model Layout

---
+++ SOFTWARE/SpringBoot/src/main/java/hr/fer/tel/server/rest/model/Layout.java
@@ -0,0 +1,44 @@
+package hr.fer.tel.server.rest.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Layout")
+public class Layout {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+
+    public Layout() {
+
+    }
+
+    public Layout(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
