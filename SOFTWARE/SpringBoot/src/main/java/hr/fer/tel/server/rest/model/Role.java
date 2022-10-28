b52be695e44aacd4f3a7de55ae18fe802898920f
lmakovec <lovro.makovec@fer.hr>
2022-10-28 11:48:33 +0200

    dodan model Role

---
+++ SOFTWARE/SpringBoot/src/main/java/hr/fer/tel/server/rest/model/Role.java
@@ -0,0 +1,44 @@
+package hr.fer.tel.server.rest.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String name;
+
+    public Role() {
+
+    }
+
+    public Role(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
