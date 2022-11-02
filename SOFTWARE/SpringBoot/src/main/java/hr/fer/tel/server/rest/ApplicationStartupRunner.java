70b6b6e2f9a5cc8f0019fd302b455e518a831f63
Filip <filip.fabris@fer.hr>
2022-11-02 11:11:59 +0100

    ApplicationStartupRunner

---
+++ SOFTWARE/SpringBoot/src/main/java/hr/fer/tel/server/rest/ApplicationStartupRunner.java
@@ -0,0 +1,31 @@
+package hr.fer.tel.server.rest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import hr.fer.tel.server.rest.service.SceneService;
+
+@Component
+public class ApplicationStartupRunner implements CommandLineRunner {
+	protected final Log logger = LogFactory.getLog(getClass());
+	
+	@Autowired
+	SceneService sceneService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Starting CommandLineRunner Init DatabBase!!");
+		
+		try{
+			sceneService.generate();
+		}catch(Exception e){
+			e.printStackTrace();
+			System.err.println("Scene generate FAILD");
+		}
+	}
+
+
+}
--- SOFTWARE/SpringBoot/src/main/java/hr/fer/tel/server/rest/RestServerApplication.java
+++ SOFTWARE/SpringBoot/src/main/java/hr/fer/tel/server/rest/RestServerApplication.java
@@ -1,36 +1,20 @@
 package hr.fer.tel.server.rest;
 
-
-import org.springframework.boot.CommandLineRunner;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import hr.fer.tel.server.rest.service.KeyService;
-import hr.fer.tel.server.rest.service.SceneService;
 
 
 //Ovo je obican init dummy vrijednostima za testiranje
 
-
 @SpringBootApplication
 public class RestServerApplication {
+	protected final Log logger = LogFactory.getLog(getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServerApplication.class, args);
 	}
 
-	
-	//U SceneService je .generate metoda
-	//Ona zove Scene static metodu koja napravi cijelu scenu
-	//Htio bih da osposobite static metodu da uspijesno doda scenu u bazu podataka
-	//Za sada keyService zanemarimo
-	@Bean
-//	@Profile("dev")
-	public CommandLineRunner init(SceneService sceneService, KeyService keyService){
-		sceneService.generate();
-//		keyService.generate();
-		return null;
-	}
+
 }
