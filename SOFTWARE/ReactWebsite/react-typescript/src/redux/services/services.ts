f99873389d0d7ae54f404f1ba2b6fe4fe7ccc08c
Ivan Kralj <kraljivan488@gmail.com>
2022-11-08 13:07:28 +0100

    task/fix-keycloak-cors

--- SOFTWARE/ReactWebsite/react-typescript/src/redux/services/services.ts
+++ SOFTWARE/ReactWebsite/react-typescript/src/redux/services/services.ts
@@ -1,65 +1,59 @@
-import apiClient from "utils/axios/apiClient";
-import { ReactTypescriptAxiosRequestConfig } from "utils/axios/apiModels";
 import { devConsoleLog } from "utils/helperFunctions";
 import { RequestConfig } from "redux/services/servicesModels";
-const FileDownload = require("js-file-download");
 
 export const executeDownloadGetRequest = async (url: string, params?: any, requestConfig?: RequestConfig) => {
-    const config: ReactTypescriptAxiosRequestConfig = {
-        params: params,
-        headers: { Authorization: `Bearer {{token}}` },
-        responseType: "blob",
-        successMessage: requestConfig?.successMessage,
-        errorMessage: requestConfig?.errorMessage,
-        useBackendErrorMessage: requestConfig?.useBackendErrorMessage,
-    };
-    await apiClient.get(url, config).then(response => {
-        let headerLine = response.headers["content-disposition"].replaceAll('"', "");
-        let startFileNameIndex = headerLine.indexOf("filename=") + "filename=".length;
-        let endFileNameIndex = headerLine.indexOf(";", startFileNameIndex);
-        let filename = headerLine.substring(startFileNameIndex, endFileNameIndex);
-        //@ts-ignore
-        FileDownload(response.data, filename != "" ? filename : "report.xlsx");
-        // FileDownload(response.data, "report.xlsx");
-    });
+    // const config: ReactTypescriptAxiosRequestConfig = {
+    //     params: params,
+    //     headers: { Authorization: `Bearer {{token}}` },
+    //     responseType: "blob",
+    //     successMessage: requestConfig?.successMessage,
+    //     errorMessage: requestConfig?.errorMessage,
+    //     useBackendErrorMessage: requestConfig?.useBackendErrorMessage,
+    // };
+    // await apiClient.get(url, config).then(response => {
+    //     let headerLine = response.headers["content-disposition"].replaceAll('"', "");
+    //     let startFileNameIndex = headerLine.indexOf("filename=") + "filename=".length;
+    //     let endFileNameIndex = headerLine.indexOf(";", startFileNameIndex);
+    //     let filename = headerLine.substring(startFileNameIndex, endFileNameIndex);
+    //     //@ts-ignore
+    //     FileDownload(response.data, filename != "" ? filename : "report.xlsx");
+    //     // FileDownload(response.data, "report.xlsx");
+    // });
 };
 
 export const executeGetRequest = async (url: string, params?: any, requestConfig?: RequestConfig) => {
-    let config: ReactTypescriptAxiosRequestConfig = {
-        params: params,
-        successMessage: requestConfig?.successMessage,
-        errorMessage: requestConfig?.errorMessage,
-        useBackendErrorMessage: requestConfig?.useBackendErrorMessage,
-    };
-    config.headers = { Authorization: `Bearer {{token}}` };
-    const response = await apiClient.get(url, config);
-
-    return response;
+    // let config: ReactTypescriptAxiosRequestConfig = {
+    //     params: params,
+    //     successMessage: requestConfig?.successMessage,
+    //     errorMessage: requestConfig?.errorMessage,
+    //     useBackendErrorMessage: requestConfig?.useBackendErrorMessage,
+    // };
+    // config.headers = { Authorization: `Bearer {{token}}` };
+    // const response = await apiClient.get(url, config);
+    // return response;
 };
 
 export const executePostRequest = async (url: string, data?: any, requestConfig?: RequestConfig) => {
-    let config: ReactTypescriptAxiosRequestConfig = {
-        successMessage: requestConfig?.successMessage,
-        errorMessage: requestConfig?.errorMessage,
-        useBackendErrorMessage: requestConfig?.useBackendErrorMessage,
-    };
-    config.headers = { Authorization: `Bearer {{token}}` };
-    const response = await apiClient.post(url, data, config);
-
-    return response;
+    // let config: ReactTypescriptAxiosRequestConfig = {
+    //     successMessage: requestConfig?.successMessage,
+    //     errorMessage: requestConfig?.errorMessage,
+    //     useBackendErrorMessage: requestConfig?.useBackendErrorMessage,
+    // };
+    // config.headers = { Authorization: `Bearer {{token}}` };
+    // const response = await apiClient.post(url, data, config);
+    // return response;
 };
 
 export const executeDeleteRequest = async (url: string, data?: any, requestConfig?: RequestConfig) => {
-    let config: ReactTypescriptAxiosRequestConfig = {
-        successMessage: requestConfig?.successMessage,
-        errorMessage: requestConfig?.errorMessage,
-        useBackendErrorMessage: requestConfig?.useBackendErrorMessage,
-        data: data,
-    };
-    config.headers = { Authorization: `Bearer {{token}}` };
-    const response = await apiClient.delete(url, config);
-
-    return response;
+    // let config: ReactTypescriptAxiosRequestConfig = {
+    //     successMessage: requestConfig?.successMessage,
+    //     errorMessage: requestConfig?.errorMessage,
+    //     useBackendErrorMessage: requestConfig?.useBackendErrorMessage,
+    //     data: data,
+    // };
+    // config.headers = { Authorization: `Bearer {{token}}` };
+    // const response = await apiClient.delete(url, config);
+    // return response;
 };
 
 export const executeRequestDirectly = async (request: Promise<any>) => {
