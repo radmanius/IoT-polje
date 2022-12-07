43c4d29722ad0959b87d90fe08785f4a22158b44
lmakovec <lovro.makovec@fer.hr>
2022-12-07 21:21:00 +0100

    Tagovi se evaluiraju u bazi, umjesto na posluzitelju

--- SOFTWARE/SpringBoot/src/main/java/hr/fer/tel/server/rest/controller/TagController.java
+++ SOFTWARE/SpringBoot/src/main/java/hr/fer/tel/server/rest/controller/TagController.java
@@ -1,10 +1,14 @@
 package hr.fer.tel.server.rest.controller;
 
+import java.util.Collection;
 import java.util.Set;
 import java.util.TreeSet;
 
 import javax.annotation.security.RolesAllowed;
 
+import hr.fer.tel.server.rest.repository.dao.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,18 +23,26 @@ public class TagController {
 
   private final TagService tagService;
 
+  @Autowired
+  private TagRepository tr;
+
   public TagController(TagService tagService) {
     this.tagService = tagService;
   }
 
-  @RolesAllowed("iot-read")
-  @GetMapping("/tags")
+  //@RolesAllowed("iot-read")
+  /*@GetMapping("/tags")
   public ResponseEntity<Set<Tag>> getTags() {
     Set<Tag> result = new TreeSet<>((o1, o2) -> o1.getName().compareTo(o2.getName()));
     tagService.getAll().stream()
       .forEach(t -> result.add(t));
 
     return ResponseEntity.ok(result);
+  }*/
+
+  @GetMapping("/tags")
+  public ResponseEntity<Collection<Tag>> getTags() {
+     return ResponseEntity.ok(tr.getAllTags());
   }
 
 }
