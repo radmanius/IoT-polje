7ce3fee415a683d589db217b0da1f6a1665c7383
josipajovic <josipa.jovic@fer.hr>
2022-12-07 13:59:38 +0100

    keycloak settings

--- SOFTWARE/flutter_client/lib/source_remote/dio/api_endpoints.dart
+++ SOFTWARE/flutter_client/lib/source_remote/dio/api_endpoints.dart
@@ -1,7 +1,11 @@
 class ApiEndpoints {
+  static String restUrl ="";
   ApiEndpoints._();
+
+
+  static set restServerBaseUrl(String v) => restUrl = v;
 
   static String get authServerBaseUrl => 'https://iotat.tel.fer.hr:58443/auth';
 
-  static String get restServerBaseUrl => 'https://iotat.tel.fer.hr:58443/rest';
+  static String get restServerBaseUrl => restUrl;
 }
--- SOFTWARE/flutter_client/lib/ui/login/login_screen.dart
+++ SOFTWARE/flutter_client/lib/ui/login/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:pdp2022/ui/common/bits/request_notifier/request_state.dart';
 import 'package:pdp2022/ui/common/primary_button.dart';
 import 'package:pdp2022/ui/home/home_screen.dart';
 import 'package:pdp2022/ui/login/provider/login_request_notifier.dart';
+import 'package:pdp2022/ui/login/settings_screen.dart';
 
 class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -26,15 +27,30 @@ class LoginScreen extends ConsumerWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: PrimaryButton(
-            title: 'LOGIN',
-            onTap: () {
-              debugPrint('pressed');
-     ref.read(loginRequestNotifier.notifier).onLoginPressed();
-}
-          ),
-        ),
+        child:Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[PrimaryButton(
+                  title: 'LOGIN',
+                  onTap: () {
+                    debugPrint('pressed');
+                    ref.read(loginRequestNotifier.notifier).onLoginPressed();
+                  }
+              ),
+                PrimaryButton(
+                    title: 'Keycloak Settings',
+                    onTap: () {
+                      debugPrint('pressed');
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const SettingsForm()),
+                      );
+                    }
+                )
+              ]),
+        )
+
       ),
     );
   }
