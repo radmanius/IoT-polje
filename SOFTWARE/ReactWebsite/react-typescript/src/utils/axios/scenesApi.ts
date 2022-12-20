83ff465248385f0ac8ea911bce4ece2ea8f168a2
Marko Brlek <marko.brlek@fer.hr>
2022-12-20 17:57:52 +0100

    Popravljena kompatibilnost sa novim backendom za scene

--- SOFTWARE/ReactWebsite/react-typescript/src/utils/axios/scenesApi.ts
+++ SOFTWARE/ReactWebsite/react-typescript/src/utils/axios/scenesApi.ts
@@ -6,7 +6,7 @@ export const getAllScenes = async () => {
     return response.data as IShortScene[]; 
 } 
 
-export const createNewScene = async (scene: IShortScene) => {
+export const createNewScene = async (scene: any) => {
     await axios.post("/scene", scene);
 };
 
@@ -16,7 +16,7 @@ export const getSceneById = async (id: number) => {
 }
 
 export const editScene = async (scene: any) => {
-    const response = await axios.put("/scene", scene);
+    const response = await axios.put("/scene/" + scene.id, scene);
     return response.data as IScene;
 };
 
