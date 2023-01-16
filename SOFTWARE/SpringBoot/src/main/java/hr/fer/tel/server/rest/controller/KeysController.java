fbf86c95c1920439efa01268c490df1edcddf6a5
izgela <ivan.zgela@fer.hr>
2023-01-16 18:29:11 +0100

    Dodani cros origin za front

--- SOFTWARE/SpringBoot/src/main/java/hr/fer/tel/server/rest/controller/KeysController.java
+++ SOFTWARE/SpringBoot/src/main/java/hr/fer/tel/server/rest/controller/KeysController.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import hr.fer.tel.server.rest.model.ShortKeyDTO;
 import hr.fer.tel.server.rest.service.KeyService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8001")
 @RequestMapping("/rest2")
 public class KeysController {
 
